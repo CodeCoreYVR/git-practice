5923417fecc5da0d7135bb44ce5f010c8a870e40
Tammam Kbeili <tammam.kbeili@gmail.com>
2015-06-15 13:09:17 -0700

    add dog class

---
+++ dog.rb
@@ -0,0 +1,30 @@
+class Dog
+
+  def initialize(color, type)
+    @color, @type = color, type
+    @bones        = []
+  end
+
+  def give(bone)
+    # ensuring that the dog has less than three bones in order to accept a new
+    # one
+    if @bones.length < 3
+      # add the bone to the bones array
+      @bones << bone
+    else
+      puts "Too many bones!"
+    end
+  end
+
+  def eat
+    if @bones.length == 0
+      puts "There are no bones!"
+    else
+      # the pop method on the array will remove and return the last element
+      # in the array. In this case @bones.pop will give us back a "bone" object
+      # the "bone" object has a "size" attribute.
+      puts "I ate a #{@bones.pop.size} bone!"
+    end
+  end
+
+end
