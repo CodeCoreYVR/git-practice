e9055e1f3a36ca7710d630d66786412744fc2727
Murphy Stroppa <murph33@gmail.com>
2015-06-15 13:44:37 -0700

    good bones

--- dog.rb
+++ dog.rb
@@ -8,7 +8,9 @@ class Dog
   def give(bone)
     # ensuring that the dog has less than three bones in order to accept a new
     # one
-    if @bones.length < 888
+
+    if @bones.length < rand(3333)
+
       # add the bone to the bones array
       @bones << bone
     else
